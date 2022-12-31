bc82ec1f9e78a89aaba7e8c9efbcfdae5fc11f8a
natreeum <projectkmscom@gmail.com>
2022-12-31 18:10:11 +0900

    commit

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-const title = document.getElementById("title");
+const loginForm = document.querySelector('#login-form');
+const loginInput = document.querySelector('#loginform input');
 
-title.innerText = "Got you!";
+function onLoginSubmit(event) {
+  event.preventDefault();
+  console.log(event);
+}
+
+loginForm.addEventListener('submit', onLoginSubmit);
