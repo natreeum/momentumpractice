1b798bd9271b631cd7c07d7912e3a6cc46dd6366
natreeum <projectkmscom@gmail.com>
2023-01-13 13:00:00 +0900

    commit

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const loginInput = document.querySelector('#loginform input');
 function onLoginSubmit(event) {
   event.preventDefault();
   console.log(event);
-  console.log('hi');
 }
 
 loginForm.addEventListener('submit', onLoginSubmit);
